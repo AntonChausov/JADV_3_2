54f1e579c4e9690d7adda848012a1d276000cd15
AntonChausov <antonchausov@gmail.com>
2021-10-10 12:55:46 +0300

    Refactor

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.LongAdder;
 
@@ -7,7 +8,7 @@ public class Main {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
 
-        FutureTask <int[]>[] myShopsResult = new FutureTask[shopCount];
+        FutureTask<int[]>[] myShopsResult = new FutureTask[shopCount];
 
         for (int i = 0; i < shopCount; i++) {
             createShop(myShopsResult, i);
@@ -17,14 +18,12 @@ public class Main {
         for (int i = 0; i < shopCount; i++) {
             getShopsResults(myShopsResult[i], report);
         }
-        System.out.println("Результат: " + report.sum());
+        System.out.println("-----\nРезультат продаж во всех магазинах: " + report.sum());
     }
 
     private static void getShopsResults(FutureTask<int[]> futureTask, LongAdder report) throws InterruptedException, ExecutionException {
         int[] shopResult = futureTask.get();
-        for (int j = 0; j < shopResult.length; j++) {
-            report.add(shopResult[j]);
-        }
+        Arrays.stream(shopResult).forEach(report::add);
     }
 
     private static void createShop(FutureTask<int[]>[] myShopsResult, int i) {
